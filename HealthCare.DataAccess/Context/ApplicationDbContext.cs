8ec3d4d733cb174f88d0179af050bdf2268019e5
Ahmed Elmaadawy <ahmed.elmaadawy03@gmail.com>
2024-08-06 22:50:57 +0300

    added aditional files

--- HealthCare.DataAccess/Context/ApplicationDbContext.cs
+++ HealthCare.DataAccess/Context/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using HealthCare.DataAccess.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HealthCare.DataAccess.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
         public ApplicationDbContext()
         {
